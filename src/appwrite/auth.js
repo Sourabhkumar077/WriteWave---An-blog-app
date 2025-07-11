68483ffb76cf5f8919606b7a36e6843f2a9aa82e
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-07-11 21:50:26 +0530

    Mega Project : initial commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,34 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount =  await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+
+            }else{
+                
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
